f2a8352c38548e76e013f44583642247db278655
Taga Studios <tagastudios@gmail.com>
2021-05-31 01:18:58 -0400

    Netlify CMS Config.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f200aecfbfb51d2f3f54";
+/******/ 	var hotCurrentHash = "8f344c0ba19eae4de5bb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
