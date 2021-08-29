d656f49fa989910f4ef0baff3c82b9597184c206
Odarlis <odarlisp@gmail.com>
2021-08-28 23:16:31 -0400

    Avioncito funcional

--- components/Home/Avioncito.js
+++ components/Home/Avioncito.js
@@ -1,20 +1,167 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Avioncito extends Component {
-  render() {
-    return (
-      <section className="about-area ptb-110">
-        <div class="grid-container">
-          <div class="grid-item item1">1</div>
-          <div class="grid-item item2">2</div>
-          <div class="grid-item item3">3</div>
-          <div class="grid-item item4">4</div>
-          <div class="grid-item item5">5</div>
-          <div class="grid-item item6">6</div>
+const Avioncito = () => {
+  const [data, setData] = useState([
+    {},
+    {
+      titulo: "Escuchar",
+      texto: "Les impartimos las instrucciones y los invitamos a jugar",
+    },
+    {
+      titulo: "Entender",
+      texto:
+        "Nos aseguramos que las instrucciones sean comprendidas por nuestros peques",
+    },
+    {
+      titulo: "Hacer",
+      texto: "manos a la obra! hacer para aprender",
+    },
+    {
+      titulo: "Vivir",
+      texto:
+        "apoyamos y acompañamos a nuestros peques a seguirlo intentando con alegrias y frustraciones hasta que lo logren.",
+    },
+    {
+      titulo: "Aprender",
+      texto: "celebramos con ellos sus logros. ",
+    },
+    {
+      titulo: "Beneficios del Juego",
+      texto:
+        "1. Proporciona mayor desarrollo sensorial -sentido y percepción 2.Aumenta la motricidad fina y gruesa 3. Estimula la memoria, atención, cognición y procesamiento lógico e imaginación 4. Facilita la comunicación, expresión, socialización  ",
+    },
+  ]);
+
+  const [play, setPlay] = useState(false);
+  const [active, setActive] = useState("");
+  return (
+    <section className="avioncito-center">
+      <div className="avioncito-left">
+        <img
+          className="avioncito-kids1 floating"
+          src="/images/avioncito/kids1.svg"
+          style={{
+            top: "0",
+            left: "0",
+            animationDelay: "0.5s",
+            duration: "8s",
+          }}
+        ></img>
+        <img
+          className="avioncito-kids2 floating"
+          src="/images/avioncito/kids2.svg"
+          style={{
+            bottom: "0",
+            right: "0",
+            animationDelay: "1s",
+            duration: "4s",
+          }}
+        ></img>
+        <img
+          className="avioncito-kids3 floating"
+          src="/images/avioncito/kids3.svg"
+          style={{
+            animationDelay: "1.5s",
+            duration: "16s",
+          }}
+        ></img>
+        <img
+          className="avioncito-kids4 floating"
+          src="/images/avioncito/kids4.svg"
+          style={{
+            bottom: "0",
+            left: "0",
+            animationDelay: "1.5s",
+            duration: "12s",
+          }}
+        ></img>
+        <img
+          className="avioncito-kids5 floating"
+          src="/images/avioncito/kids5.svg"
+          style={{
+            top: "0",
+            right: "0",
+            animationDelay: "2s",
+            duration: "10s",
+          }}
+        ></img>
+      </div>
+      <div class="grid-container">
+        <div
+          className="grid-item item1"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[1]);
+          }}
+        >
+          1
         </div>
-      </section>
-    );
-  }
-}
+        <div
+          className="grid-item item2"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[2]);
+          }}
+        >
+          2
+        </div>
+        <div
+          className="grid-item item3"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[3]);
+          }}
+        >
+          3
+        </div>
+        <div
+          className="grid-item item4"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[4]);
+          }}
+        >
+          4
+        </div>
+        <div
+          className="grid-item item5"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[5]);
+          }}
+        >
+          5
+        </div>
+        <div
+          className="grid-item item6"
+          onClick={() => {
+            setPlay(true);
+            setActive(data[6]);
+          }}
+        >
+          6
+        </div>
+      </div>
+      {play ? (
+        <div
+          className="avioncito-right"
+          style={{ justifyContent: "Flex-start" }}
+        >
+          <h2>{active.titulo}</h2>
+          <p>{active.texto}</p>
+        </div>
+      ) : (
+        <div className="avioncito-right">
+          <h2> Cómo enseñamos en Magic Mind? </h2>
+          <h3>
+            Para conocer nuestros Magic Métodos, Vamos a jugar el avioncito
+          </h3>
+          <p> Te mostramos nuestras estratégias en 5 pasos!</p>
+          <img src="/images/avioncito/kids6.svg"></img>
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default Avioncito;
--- components/Home/MainServices.js
+++ components/Home/MainServices.js
@@ -12,19 +12,14 @@ const MainServices = () => {
                 <i className="flaticon-robot-1"></i>
               </div>
               <h3>
-                <Link href="/#">
-                  <a>PlayPals</a>
+                <Link href="/services/playpals/">
+                  <a>Categorías de Enseñanza</a>
                 </Link>
               </h3>
-              <p>
-                PlayPals son personas calificadas que irán a jugar, estimular y
-                enseñar a su hijo en la comodidad de su hogar. No es nanny, ni
-                babysitter, es un compañero de juego. Usted puede elegir entre
-                varias actividades como, arte, idiomas, música y/o entro otros.
-              </p>
+              <img src="/images/Helpfull-links/Categorias.png"></img>
               <br />
 
-              <Link href="/#">
+              <Link href="services/playpals/">
                 <a className="link-btn">Leer más..</a>
               </Link>
             </div>
@@ -36,19 +31,30 @@ const MainServices = () => {
                 <i className="flaticon-neural"></i>
               </div>
               <h3>
-                <Link href="/#">
-                  <a>Actividades Personalizadas</a>
+                <Link href="/blogs/">
+                  <a>Tips Para Papás</a>
                 </Link>
               </h3>
-              <p>
-                Nuestro Playpal planificará los juegos de acuerdo a las
-                necesidades de su hijo para que aprenda de manera divertida, los
-                niños prefieren jugar que estudiar y es por eso que aprenderemos
-                jugando! Los niños no juegan para aprender, pero de manera más o
-                menos consciente, aprenden jugando.
-              </p>
+              <img src="/images/Helpfull-links/Tips.png"></img>
 
-              <Link href="/#">
+              <Link href="/blogs/">
+                <a className="link-btn">Leer más..</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-4 col-sm-12 col-md-6 offset-lg-0 offset-md-3 offset-sm-0">
+            <div className="single-main-services-box">
+              <div className="icon">
+                <i className="flaticon-machine-learning"></i>
+              </div>
+              <h3>
+                <Link href="/about-us/">
+                  <a>Nuestro Equipo</a>
+                </Link>
+              </h3>
+              <img src="/images/Helpfull-links/team.png"></img>
+              <br />
+              <Link href="/about-us/">
                 <a className="link-btn">Leer más..</a>
               </Link>
             </div>
@@ -61,17 +67,47 @@ const MainServices = () => {
               </div>
               <h3>
                 <Link href="/#">
-                  <a>Productos</a>
+                  <a>Planilla de Inscripción</a>
                 </Link>
               </h3>
-              <p>
-                Libros, videos, juguetes, cada uno de nuestros productos
-                enfocados en los padres, considerando el rol familiar dentro de
-                la formacion infantil y en busqueda de un beneficio para el
-                desarrollo de los niños.
-              </p>
+              <img src="/images/Helpfull-links/Planilla.png"></img>
               <br />
               <Link href="/#">
+                <a className="link-btn">Leer más..</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-4 col-sm-12 col-md-6 offset-lg-0 offset-md-3 offset-sm-0">
+            <div className="single-main-services-box">
+              <div className="icon">
+                <i className="flaticon-machine-learning"></i>
+              </div>
+              <h3>
+                <Link href="/about-us/">
+                  <a>Nuestro Equipo</a>
+                </Link>
+              </h3>
+              <img src="/images/Helpfull-links/team.png"></img>
+              <br />
+              <Link href="/about-us/">
+                <a className="link-btn">Leer más..</a>
+              </Link>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-sm-12 col-md-6 offset-lg-0 offset-md-3 offset-sm-0">
+            <div className="single-main-services-box">
+              <div className="icon">
+                <i className="flaticon-machine-learning"></i>
+              </div>
+              <h3>
+                <Link href="/contact/">
+                  <a>Más Información</a>
+                </Link>
+              </h3>
+              <img src="/images/Helpfull-links/informacion.png"></img>
+              <br />
+              <Link href="/contact/">
                 <a className="link-btn">Leer más..</a>
               </Link>
             </div>
