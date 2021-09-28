0a71120f2db9352b2b6bfbfe8e36d2693361e0c3
Taga Studios <tagastudios@gmail.com>
2021-09-28 00:04:16 -0400

    Imagenes, leerMas

--- components/Home/ProfessionalSolutions.js
+++ components/Home/ProfessionalSolutions.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const ProfessionalSolutions = () => {
+  const text = {
+    corto:
+      "Los niños cuando llegan al mundo son lienzos en blanco y se convertirán en una persona u otra dependiendo de...",
+    largo:
+      "Los niños cuando llegan al mundo son lienzos en blanco y se convertirán en una persona u otra dependiendo de la educación que haya recibido y del entorno en el que crezca. Ellos pueden absorber mucha información cuando son pequeños, el amor incondicional y el cariño no pueden faltarles para su buen desarrollo, pero a medida que crecen y se van desarrollando, es necesario centrarse en la educación",
+  };
+  const [leerMas, setLeerMas] = useState(false);
   return (
     <div className="solutions-area ptb-110 MainBanner__fix2">
       <div className="container">
         <div className="section-title letras">
-          <p className="letras2">
-            {" "}
-            "Los niños cuando llegan al mundo son lienzos en blanco y se
-            convertirán en una persona u otra dependiendo de la educación que
-            haya recibido y del entorno en el que crezca. Ellos pueden absorber
-            mucha información cuando son pequeños, el amor incondicional y el
-            cariño no pueden faltarles para su buen desarrollo, pero a medida
-            que crecen y se van desarrollando, es necesario centrarse en la
-            educación"
-          </p>
-          <p></p>
+          <p className="letras2"> </p>
+          <p>{leerMas ? text.largo : text.corto}</p>
+          <a onClick={() => setLeerMas(!leerMas)}>
+            {leerMas ? "Leer menos" : "Leer mas"}
+          </a>
         </div>
       </div>
     </div>
