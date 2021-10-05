7d3c7248f082b63b976a554935a98505af51dc6a
Odarlis <odarlisp@gmail.com>
2021-10-05 10:39:27 -0400

    Testimonios

--- components/Home/Testimonials.js
+++ components/Home/Testimonials.js
@@ -43,10 +43,16 @@ class FeedbackSlider extends Component {
                 <div className="col-lg-6 col-md-12 p-0">
                   <div className="feedback-item testimonials-fix">
                     <p>
-                      Las maestras y el personal administrativo es muy
-                      profesional y atento. A mi hijo le encanta y siempre está
-                      emocionado en cada lección. Y el nivel de aprendizaje es
-                      excelente..
+                      Trabajar con Maryangelica fue una de las mejores
+                      experiencias no solo laborales, pero sobre todo en nivel
+                      de crecimiento personal, aprendí tantas cosas que ni las
+                      puedo nombrar! Pero aparte de cambiar pañales (que aun no
+                      he necesitado) sobre todo aprendí mucho de psicología de
+                      un niño, de las cuales no tuve ni idea.. Entendí que los
+                      niños llaman atención de formas (no siempre comodas)
+                      cuando les hace falta, pero es una 'alerta' q necesita
+                      nuestra atencion y corrección. Supe manejar muchisimas
+                      situaciones gracias a esa experiencia.
                     </p>
 
                     <div className="client-info">
@@ -54,8 +60,8 @@ class FeedbackSlider extends Component {
                         <img src="/images/clients/client1.jpg" alt="image" />
                       </div>
 
-                      <h3>Filip Luis</h3>
-                      <span>MagicPadres</span>
+                      <h3>Dasa Orsagova</h3>
+                      <span>Team</span>
                     </div>
                   </div>
                 </div>
@@ -71,12 +77,17 @@ class FeedbackSlider extends Component {
                 <div className="col-lg-6 col-md-12 p-0">
                   <div className="feedback-item testimonials-fix">
                     <p>
-                      Mi hija ingresó desde los 2 años. Como madre primeriza
-                      sentí muchísimo miedo de dejarla tan pequeña con personas
-                      que no conocía! Hoy después de casi un año puedo compartir
-                      con ustedes mi sentimiento de felicidad cada vez que veo a
-                      mi hija sonriente, bajo un conocimiento cuidado y cariño
-                      excepcional, recomiendo MagicMind con los ojos cerrados!
+                      La parte dificil, no son los niños, somos los padres.. Y
+                      como lidiar con eso entre dos, porque entre dos no siempre
+                      vemos, entendemos o acordamos lo mismo (y es normal).
+                      Muchas veces deseo que por lo menos algun fragmento de esa
+                      experiencia de Maryangelica se pudiera pasar para mi
+                      esposa jajaja pero pienso que todos estamos aprendiendo en
+                      el camino mas que todo y obviamente hay muchas cosas que
+                      no se pueden aprender, solo vivir y resolver. Pero
+                      regularmente se me vienen recuerdos de esos tiempos,
+                      porque era una escuela de la vida, hasta creo que mas para
+                      nosotros los teachers que para los niños :)
                     </p>
 
                     <div className="client-info">
@@ -84,8 +95,8 @@ class FeedbackSlider extends Component {
                         <img src="/images/clients/client2.jpg" alt="image" />
                       </div>
 
-                      <h3>Sarah Taylor</h3>
-                      <span>MagicPadres</span>
+                      <h3>Pedro Perez</h3>
+                      <span>Team</span>
                     </div>
                   </div>
                 </div>
@@ -101,9 +112,16 @@ class FeedbackSlider extends Component {
                 <div className="col-lg-6 col-md-12 p-0">
                   <div className="feedback-item testimonials-fix">
                     <p>
-                      ¡Gran escuela, por favor! Los profesores son increíbles,
-                      todos lo son, a mi hija le encanta y la enseñanza es otro
-                      nivel.
+                      Maryan es una bendición!!! Contar con Maryan como apoyo
+                      desde mi primer día como mamá ha sido siempre una
+                      bendición, su conocimiento sobre el tema es importante y
+                      tranquilizador cuando te explica las teorías, pero la
+                      empatía con la que siempre ha recibido mis angustias e
+                      inquietudes y el tono de normalidad que le da a cada uno
+                      de los momentos en los cuales he sentido que estoy
+                      fallando como mamá o me he sentido perdida sin saber que
+                      hacer ha sido definitivamente el mejor de sus aportes en
+                      mi vida de mamá.
                     </p>
 
                     <div className="client-info">
@@ -111,7 +129,7 @@ class FeedbackSlider extends Component {
                         <img src="/images/clients/client3.jpg" alt="image" />
                       </div>
 
-                      <h3>Sarah Taylor</h3>
+                      <h3>Adelaida Pifano</h3>
                       <span>MagicPadres</span>
                     </div>
                   </div>
